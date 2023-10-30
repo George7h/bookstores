5ac443da7a68d9343a588b8dc7ff33044be4653f
George <georgie.george7@gmail.com>
2023-10-30 17:22:54 +0400

    add books to initial state

--- src/redux/books/booksSlice.js
+++ src/redux/books/booksSlice.js
@@ -1,26 +1,34 @@
 import { createSlice } from '@reduxjs/toolkit';
 
-export const booksSlice = createSlice({
-  name: 'books',
-  initialState: {
-    books: [],
+const initialState = [
+  {
+    id: 'item1',
+    title: 'The Great Gatsby',
+    author: 'John Smith',
+    category: 'Fiction',
   },
+  {
+    id: 'item2',
+    title: 'Anna Karenina',
+    author: 'Leo Tolstoy',
+    category: 'Fiction',
+  },
+  {
+    id: 'item3',
+    title: 'The Selfish Gene',
+    author: 'Richard Dawkins',
+    category: 'Nonfiction',
+  },
+];
+
+const booksSlice = createSlice({
+  name: 'books',
+  initialState,
   reducers: {
     addBook: (state, action) => {
-      const { title, author, category } = action.payload;
-      state.books = [
-        ...state.books,
-        {
-          id: Math.floor(Math.random() * 100),
-          title,
-          author,
-          category,
-        },
-      ];
+      state.push(action.payload);
     },
-    removeBook: (state, action) => {
-      state.books = state.books.filter((book) => book.id !== action.payload);
-    },
+    removeBook: (state, action) => state.filter((book) => book.id !== action.payload),
   },
 });
 
