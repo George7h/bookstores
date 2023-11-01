d4ddfe1320210942d76162a45bd462f318df26f3
George <georgie.george7@gmail.com>
2023-11-01 12:58:33 +0400

    Fix delete functionality

--- src/components/deleteBookButton.js
+++ src/components/deleteBookButton.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const DeleteBookButton = ({ onClick, bookId }) => (
-  <button type="button" onClick={() => onClick(bookId)}>
+const DeleteBookButton = ({ onClick, itemId }) => (
+  <button type="button" onClick={() => onClick(itemId)}>
     Delete
   </button>
 );
 
 DeleteBookButton.propTypes = {
   onClick: PropTypes.func.isRequired,
-  bookId: PropTypes.number.isRequired,
+  itemId: PropTypes.string.isRequired,
 };
 
 export default DeleteBookButton;
